8d6d5d1298237995c7fab4f97bbe1a24ef0dc5b8
Vecto <system.bot.discord.utility@gmail.com>
2024-04-26 15:42:03 +0200

    v0.0.0

--- src/index.js
+++ src/index.js
@@ -1,65 +1,43 @@
-require('dotenv').config();
-const express = require('express');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const fs = require('fs');
-const path = require('path');
-const http = require('http');
-const https = require('https');
-const session = require('express-session');
-const bodyParser = require('body-parser');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/api');
 
-const ejs = require('ejs');
+var app = express();
 
-const PORT = process.env.PORT || 8080;
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-const app = express();
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/api', apiRouter);
+
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+    next(createError(404));
 });
 
-app.use(session({
-    secret: 'your-secret-key',
-    resave: false,
-    saveUninitialized: false
-}));
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-// Statische Dateien
-app.use('/static', express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.set('views', path.join(__dirname, '/views'));
-app.engine('html', ejs.renderFile);
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-    res.render('index', { title: 'Home' });
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
-//app.get('/about', (req, res) => {
-//    res.render('about', { title: 'About' });
-//});
-
-//app.get('/contact', (req, res) => {
-//    res.render('contact', { title: 'Contact' });
-//});
-
-//app.get('/login', (req, res) => {
-//    res.render('login', { title: 'Login' });
-//});
-
-//app.get('/register', (req, res) => {
-//    res.render('register', { title: 'Register' });
-//});
-
-//app.get('/logout', (req, res) => {
-//    req.session.destroy();
-//    res.redirect('/');
-//});
-
-
-app.listen(PORT, () => {
-    console.log(`Example App is listening on port ${PORT}`);
-});
+module.exports = app;
